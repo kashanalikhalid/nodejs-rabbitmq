35f3845a147eeb7d9385a0d78f34310c6a610f99
Kashan Ali Khalid <kashanali_khalid@yahoo.com>
2022-02-24 12:55:38 +0500

    nodejs with rabbitmq on local docker

--- receive.js
+++ receive.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get('/:data', (req, res) => {
 
     amqp.connect('amqp://rabbit',  function(error0, connection) {
         if (error0) {
